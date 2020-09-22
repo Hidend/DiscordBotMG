298fab2b1d6733e03f766908a948ff65080d4a2c
hdnd <hdnd@protonmail.com>
2020-09-22 21:56:15 +0200

    Fix conflicts

---
+++ Modules/Utility/HelpCommand.cs
@@ -0,0 +1,90 @@
+﻿using Discord;
+using Discord.Commands;
+using DiscordBotMG.Utils;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBotMG.Modules.Administration
+{
+	public class HelpCommand : ModuleBase<SocketCommandContext>
+    {
+        private readonly CommandService _service;
+        private string prefix = ServerConfig.prefix;
+
+        public HelpCommand(CommandService service)
+        {
+            _service = service;
+        }
+
+        [Command("help")]
+        [Summary("Te devuelve la ayuda de todos los comandos del Bot")]
+        public async Task HelpAsync()
+        {
+            var builder = new EmbedBuilder()
+            {
+                Color = new Color(114, 137, 218),
+                Description = "Comandos que puedes usar:"
+            };
+
+            foreach (var module in _service.Modules)
+            {
+                string description = null;
+                foreach (var cmd in module.Commands)
+                {
+                    var result = await cmd.CheckPreconditionsAsync(Context);
+                    if (result.IsSuccess)
+                        description += $"{prefix}{cmd.Aliases.First()} {string.Join(", ", cmd.Parameters.Select(p => p.Name))}\n";
+                }
+
+                if (!string.IsNullOrWhiteSpace(description))
+                {
+                    builder.AddField(x =>
+                    {
+                        x.Name = module.Name;
+                        x.Value = description;
+                        x.IsInline = false;
+                    });
+                }
+            }
+
+            await ReplyAsync("", false, builder.Build());
+        }
+        [Command("help")]
+        [Summary("Te devuelve la ayuda de un comando del Bot")]
+        public async Task HelpAsync(string command)
+        {
+            var result = _service.Search(Context, command);
+
+            if (!result.IsSuccess)
+            {
+                await ReplyAsync($"No encontrado **{command}**.");
+                return;
+            }
+
+            var builder = new EmbedBuilder()
+            {
+                Color = new Color(114, 137, 218),
+                Description = $"Comandos de **{command}**"
+            };
+
+            foreach (var match in result.Commands)
+            {
+                var cmd = match.Command;
+
+                builder.AddField(x =>
+                {
+                    x.Name = string.Join(", ", cmd.Aliases);
+                    x.Value = $"Parametros: {string.Join(", ", cmd.Parameters.Select(p => p.Name))}\n" +
+                              $"Descripción: {cmd.Summary}";
+                    x.IsInline = false;
+                });
+            }
+
+            await ReplyAsync("", false, builder.Build());
+        }
+    }
+}
